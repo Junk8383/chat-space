a1706856b8c0c1ec9c91ff8a5ce8f475fedd2842
@Junk8383 <sksk14@icloud.com>
2019-12-09 18:34:00 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'junk8383'
-  config.asset_host = 'https://s3-ap-northeast-2.amazonaws.com/junk8383'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/junk8383'
 end
